639673b2976a915589697e43199acc049868caac
Corbin-Creedon <corbinm.creedon@gmail.com>
2020-12-31 03:05:02 +0000

    Update App.js

--- React/src/containers/App/App.js
+++ React/src/containers/App/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <h1>Hello from V2</h1>
       </header>
     </div>
