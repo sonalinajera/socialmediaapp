199780a83dad9b671b555f5b78079dbf95f96db4
Sonali Najera <sonalitzul@gmail.com>
2020-12-30 19:44:12 -0800

    adding nav bar

--- React/src/containers/App/App.js
+++ React/src/containers/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { LoginForm } from '../../components/LoginForm/LoginForm';
+import Navbar from '../../components/Navbar/Navbar';
 import './App.css';
 
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <LoginForm />
     </div>
   )
