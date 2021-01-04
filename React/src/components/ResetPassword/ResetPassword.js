33d6aa9ba4d2e1045afb0952a841b4ee2b7c44d7
Orkun Ozekin <orkunozekin@gmail.com>
2021-01-03 21:57:20 -0600

    refactored resetpassword component

--- React/src/components/ResetPassword/ResetPassword.js
+++ React/src/components/ResetPassword/ResetPassword.js
@@ -11,30 +11,38 @@ function ResetPassword(props) {
     const serviceID = 'service_0nmad6w';
     const templateID = 'template_2bp3804';
 
-    const sendMail = () => {
-        let userEmail = document.getElementById('to-email').value;
 
+    //Before sending an email to the user, we need to look through the emails in our db and ensure that this user exists
+    const sendMail = (ev) => {
+        //ev.preventDefault() prevents the browser from reloading the page. This is only necessary for forms.
+        ev.preventDefault()
+        //Utilize object destructuring to get the input from the form where this function will be invoked. 
+        const { userEmail } = ev.target;
+        //.value is the actual user's input as a string.
+        console.log(userEmail.value)
         let templateParams = {
-            to_email: userEmail,
+            to_email: userEmail.value,
             url: 'www.google.com'
         };
 
         emailjs.send(serviceID, templateID, templateParams)
-
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
+                //reset the input box
+                userEmail.value = ''
             }, function (error) {
                 console.log('FAILED...', error);
             })
+
     };
 
 
     return (
         <div className="reset-form-wrapper">
-            <Form className="reset-form">
-                <Form.Control type="email" id="to-email" placeholder="Enter email" />
+            <Form onSubmit={(ev) => sendMail(ev)} className="reset-form">
+                <Form.Control name="userEmail" type="email" id="to-email" placeholder="Enter email" />
                 <br></br>
-                <Button onClick={sendMail} variant="primary" type="submit">
+                <Button variant="primary" type="submit">
                     Send
                 </Button>
             </Form >
