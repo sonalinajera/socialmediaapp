02cc494bea0547e1d99cefe0e38e370464483bf4
Orkun Ozekin <orkunozekin@gmail.com>
2021-01-04 22:28:36 -0600

    implemented notFoundPage

--- React/src/containers/App/App.js
+++ React/src/containers/App/App.js
@@ -6,7 +6,7 @@ import LoginPage from '../../routes/LoginPage/LoginPage'
 import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
 import Email from '../../components/ResetPassword/Email/Email'
 import ResetPassword from '../../components/ResetPassword/ResetPassword/ResetPassword'
-
+import NotFoundRoute from '../../routes/NotFoundRoute/NotFoundRoute'
 
 
 const App = () => {
@@ -25,6 +25,7 @@ const App = () => {
         <Route exact path='/user/registration' component={RegistrationPage} />
         <Route exact path='/user/email-reset-password' component={Email} />
         <Route exact path='/user/reset-password' component={ResetPassword} />
+        <Route component={NotFoundRoute} />
       </Switch>
     </div>
   )
