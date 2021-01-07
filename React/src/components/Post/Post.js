ffd9ccb288a384825e15133efa2b584b1a310301
Sonali Najera <sonalitzul@gmail.com>
2021-01-07 12:42:16 -0800

    added functunality to create post button. dynamically shows and hides post form

--- React/src/components/Post/Post.js
+++ React/src/components/Post/Post.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Button, Form } from 'react-bootstrap';
 import './Post.css'
+import WritePost from '../../images/icons/writePost.png'
 
 const Post = () => {
+  const [shouldDisplayPostForm, displayPostForm] = useState(false);
   return (
     <Container className="newPostContainer">
-      <Button variant="light">Create a new post!</Button>
-      <Form>
+      <Button variant="light" onClick={() => {displayPostForm(!shouldDisplayPostForm)}} className={shouldDisplayPostForm ? "hidden" : ""}><img src={WritePost}/> create a new post!</Button>
+       <Form className={shouldDisplayPostForm ? "" : "hidden"}>
+         <h2>Create new Post</h2>
         <Form.Group controlId="exampleForm.ControlTextarea1">
           <Form.Label>Tell us about it Janet!</Form.Label>
           <Form.Control as="textarea" rows={3} />
