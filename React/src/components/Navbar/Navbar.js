f52a22ef6988860c90840745396fc88a36dd5504
Sonali Najera <sonalitzul@gmail.com>
2021-01-07 14:21:59 -0800

    fixed profile routes in app js

--- React/src/components/Navbar/Navbar.js
+++ React/src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ const Nav = () => {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/user/profile">Home</a>
+                <a className="nav-link active" aria-current="page" href="/user/home">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Link</a>
--- React/src/components/Post/Post.js
+++ React/src/components/Post/Post.js
@@ -5,11 +5,13 @@ import WritePost from '../../images/icons/writePost.png'
 
 const Post = () => {
   const [shouldDisplayPostForm, displayPostForm] = useState(false);
+  const [shouldDisplayPostButton, displayPostButton] = useState(true);
+
   return (
     <Container className="newPostContainer">
-      <Button variant="light" onClick={() => {displayPostForm(!shouldDisplayPostForm)}} className={shouldDisplayPostForm ? "hidden" : ""}><img src={WritePost}/> create a new post!</Button>
+      <Button variant="light" onClick={() => {displayPostForm(!shouldDisplayPostForm)}} className={shouldDisplayPostButton ? "hidden" : ""}><img src={WritePost}/> create a new post!</Button>
        <Form className={shouldDisplayPostForm ? "" : "hidden"}>
-         <h2>Create new Post</h2>
+         <h2>Create new Post <Button variant="light">X</Button></h2>
         <Form.Group controlId="exampleForm.ControlTextarea1">
           <Form.Label>Tell us about it Janet!</Form.Label>
           <Form.Control as="textarea" rows={3} />
