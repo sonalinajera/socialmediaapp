7ce087487a382781f51dd333dd37335e9b06dc48
Orkun Ozekin <orkunozekin@gmail.com>
2021-01-08 21:50:35 -0600

    navbar refactored

--- React/src/components/Navbar/Navbar.js
+++ React/src/components/Navbar/Navbar.js
@@ -1,36 +1,25 @@
 import React from 'react'
 import SearchBar from '../SearchBar/SearchBar'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Search } from '@material-ui/icons'
 
-const Nav = () => {
+const NavigationBar = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">UserName</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/user/home">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled (Test)</a>
-              </li>
-
-             
-            </ul>
-            <SearchBar />
-          </div>
-        </div>
-      </nav>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <SearchBar />
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   )
 }
 
-export default Nav
+export default NavigationBar
