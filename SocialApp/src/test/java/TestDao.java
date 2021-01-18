0922fac214103c14c535986dbfef4fca8b33a588
Sonali Najera <sonalitzul@gmail.com>
2021-01-17 17:03:03 -0800

    implemented update email

--- SocialApp/src/test/java/TestDao.java
+++ SocialApp/src/test/java/TestDao.java
@@ -1,20 +1,20 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
-import org.hibernate.SessionFactory;
-import org.junit.Test;
-
-import com.socialapp.dao.UserRepo;
-
-public class TestDao {
-	
-	private SessionFactory sesFact;
-	
-	@Test
-	public void test() {
-		UserRepo userRepo = new UserRepo(sesFact);
-		
-		assertNull(userRepo.selectUserByEmail("orkunozekin@gmail.com"));
-	}
-
-}
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNull;
+//
+//import org.hibernate.SessionFactory;
+//import org.junit.Test;
+//
+//import com.socialapp.dao.UserRepo;
+//
+//public class TestDao {
+//	
+//	private SessionFactory sesFact;
+//	
+//	@Test
+//	public void test() {
+//		UserRepo userRepo = new UserRepo(sesFact);
+//		
+//		assertNull(userRepo.selectUserByEmail("orkunozekin@gmail.com"));
+//	}
+//
+//}
