ec5ab3782d09ec9d6e47b735283fe7d8171290a6
Tim Hammes <t.p.hammes@gmail.com>
2021-01-18 18:21:47 -0700

    Revert "Bio card connections no longer hard coded"
    This reverts commit 93c270437488ceccaeeb5bb2bbf19dcb549793ae.

--- React/src/routes/HomePage/HomePage.js
+++ React/src/routes/HomePage/HomePage.js
@@ -8,24 +8,24 @@ import { useHistory } from "react-router-dom";
 
 const HomePage = (props) => {
 
-    //router props history to change the URI.
-    let history = useHistory();
-    console.log("users:" + props.userData.length);
-    if (TokenService.hasAuthToken()) {
-        return (
-            <div className="homepage">
-                <BioCard uData={props.userData} />
-                <Post />
-                {props.userData.map((user, index) => {
-                    return user.posts.map((post, key) => {
-                        return (<PostContents key={key} pData={post} uData={user} numOfUsers={props.userData.length} />)
-                    })
-                })}
-            </div>
-        )
-    } else {
-        history.push('/');
-    }
+  //router props history to change the URI.
+  let history = useHistory();
+
+  if (TokenService.hasAuthToken()) {
+    return (
+      <div className="homepage">
+        <BioCard />
+        <Post />
+        {props.userData.map((user, index) => {
+          return user.posts.map((post, key) => {
+            return (<PostContents key={key} pData={post} uData={user} numOfUsers={props.userData.length} />)
+          })
+        })}
+      </div>
+    )
+  } else {
+    history.push('/');
+  }
 }
 
 export default HomePage;
